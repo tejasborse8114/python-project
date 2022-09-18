40466478c69ad691ce4fbb441cbf3d3070f2dec5
Tejas Borse <tejasborse8114@gmail.com>
2022-09-18 15:50:59 +0530

    python project

---
+++ views.py
@@ -0,0 +1,54 @@
+from urllib import request
+
+import Fibonacci as Fibonacci
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("main:homepage")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="main/register.html", context={"register_form": form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="main/login.html", context={"login_form": form})
+
+
+def home(n):
+        if n < 0:
+            print("Incorrect input")
+        elif n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+
+        else:
+            return Fibonacci(n - 1) + Fibonacci(n - 2)
+    return render(request, "home.html", {"result": result})
